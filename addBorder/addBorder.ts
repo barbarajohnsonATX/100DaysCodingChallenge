93f026ce88d8f0aff37949867889ea62810e6541
barbarajohnsonATX <johnson.barbara@gmail.com>
2020-05-19 20:28:30 -0500

    Done

--- addBorder/addBorder.ts
+++ addBorder/addBorder.ts
@@ -1,5 +1,14 @@
 function addBorder(picture: string[]): string[] {
+    let result = []
+    for(let i=0; i < picture.length ;  i++) {
+        result.push(('*').concat(picture[i], '*'))
+    }
 
+     result.push('*'.repeat(picture[0].length + 2))
+     result.unshift('*'.repeat(picture[0].length + 2))
+
+     return result
+ 
 }
 
-// console.log(addBorder(["abc", "ded"]));
+ console.log(addBorder(["abc", "ded"]));
