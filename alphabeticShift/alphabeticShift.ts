21b4724a950b4534068622bb72aa5550cd0548d6
barbarajohnsonATX <johnson.barbara@gmail.com>
2020-05-19 22:41:30 -0500

    Done

--- alphabeticShift/alphabeticShift.ts
+++ alphabeticShift/alphabeticShift.ts
@@ -1,5 +1,17 @@
 function alphabeticShift(inputString: string): string {
+    let shifted = []
 
+ 
+    inputString.split('').forEach(letter => {
+        //z becomes a, Z becomes A
+        let shiftedCode = letter.charCodeAt(0) === 'z'.charCodeAt(0) ||  letter.charCodeAt(0) === 'Z'.charCodeAt(0)  ? letter.charCodeAt(0) - 25 : letter.charCodeAt(0) + 1
+        console.log(String.fromCharCode(shiftedCode))            
+        shifted.push(String.fromCharCode(shiftedCode))
+
+ 
+    })
+    return shifted.join('')
 }
 
 console.log(alphabeticShift('crazy'));
+console.log(alphabeticShift('Zig'));
