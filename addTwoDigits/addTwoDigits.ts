e00494b9524620a9f11fbb24d8fab710a7d3567c
barbarajohnsonATX <johnson.barbara@gmail.com>
2020-05-19 20:36:07 -0500

    Done

--- addTwoDigits/addTwoDigits.ts
+++ addTwoDigits/addTwoDigits.ts
@@ -1,5 +1,7 @@
 function addTwoDigits(n: any): number {
+    return Math.floor(n / 10) + n % 10 
 
 }
 
-// console.log(addTwoDigits(29));
+ console.log(addTwoDigits(29));
+ console.log(addTwoDigits(21));
