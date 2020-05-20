fd0b356da27b40f592f9adc3b26fdcf00c54d326
barbarajohnsonATX <johnson.barbara@gmail.com>
2020-05-19 21:44:40 -0500

    Done

--- almostIncreasingSequence/almostIncreasingSequence.ts
+++ almostIncreasingSequence/almostIncreasingSequence.ts
@@ -1,6 +1,19 @@
 function almostIncreasingSequence(sequence: number[]): boolean {
 
+    let incorrect = 0; 
+
+    for(let i = 0; i < sequence.length - 1; i++) {
+        let isIncreasing = sequence[i] < sequence[i+1] ? true : false 
+        if (!isIncreasing) {
+            incorrect++;
+        }
+    }
+        if(incorrect > 1) {
+            return false;
+        }
+        return true;
+
 }
 
-// console.log(almostIncreasingSequence([1, 3, 2, 1])) 
-// console.log(almostIncreasingSequence([1, 3, 2])) 
+ console.log(almostIncreasingSequence([1, 3, 2, 1])) 
+ console.log(almostIncreasingSequence([1, 3, 2])) 
