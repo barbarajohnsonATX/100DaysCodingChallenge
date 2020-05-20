c97185d380a67a0a72916a25509a65be8b0aac8d
barbarajohnsonATX <johnson.barbara@gmail.com>
2020-05-20 00:09:48 -0500

    Done

--- alternatingSums/alternatingSums.ts
+++ alternatingSums/alternatingSums.ts
@@ -1,5 +1,18 @@
 function alternatingSums(a: number[]): number[] {
+    let sums = [0, 0]; 
+ 
+    for(let i = 0; i < a.length ; i++) {
+        if(!(i % 2)) {
+            sums[0] += a[i]
+        } else {
+            sums[1] += a[i]
+        }
+    }
+
+        
+     return sums
 
 }
 
 console.log(alternatingSums([50, 60, 60, 45, 70]))
+console.log(alternatingSums([50, 60, 60, 45, 70, 15]))
