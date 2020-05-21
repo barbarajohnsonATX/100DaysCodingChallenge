6b0ba40f0a0a300884c1b975622b90e7c92d2b4f
barbarajohnsonATX <johnson.barbara@gmail.com>
2020-05-21 00:16:30 -0500

    Done

--- arrayChange/arrayChange.ts
+++ arrayChange/arrayChange.ts
@@ -1,5 +1,17 @@
 function arrayChange(inputArray: number[]): number {
+    let moves = 0;
+ 
+    
+    for(let i  = 1; i < inputArray.length; i++) {
+            if(inputArray[i] !== inputArray[i-1] + 1) {
+                moves += Math.abs(inputArray[i] - inputArray[i-1]) + 1
+                inputArray[i] = inputArray[i - 1] + 1
+              }
+ 
+    }
+    return moves 
  
 }
 
 console.log(arrayChange([1, 1, 1]));
+console.log(arrayChange([2, 1, 1]));
