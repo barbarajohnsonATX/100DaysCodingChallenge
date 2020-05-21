0c918cd30ffecf5afa12f0b32a0e1cb6d7964330
barbarajohnsonATX <johnson.barbara@gmail.com>
2020-05-20 23:03:22 -0500

    Done

---
+++ longestSubSequence/longestSubSequence.ts
@@ -0,0 +1,31 @@
+function longestSubSequence(s1: string, s2: string): string {
+
+    let result1 = []  
+    let S1 = s1.split('')
+    let S2 = s2.split('')
+
+    let n2Start = 0
+     for(let i = 0; i < S1.length; i++) {
+        for(let j = n2Start; j < S2.length; j++) {
+            if(S1[i] === S2[j]) {
+                result1.push(S2[j])
+                n2Start++
+                i++
+            }
+            
+        }
+        
+    }
+
+    
+
+
+     return result1.join(',')
+}
+
+console.log(longestSubSequence('abcdehfg', 'zbxxedhhg'))
+
+console.log(longestSubSequence('abazdc', 'bacbad'))
+console.log(longestSubSequence('aaa', 'aa'))
+console.log(longestSubSequence('abba', 'abcaba'))
+
