feb72c9ae78a2d6ea13749ed0e0d7918a4971014
barbarajohnsonATX <johnson.barbara@gmail.com>
2020-05-22 23:02:42 -0500

    Done

--- arrayReplace/arrayReplace.ts
+++ arrayReplace/arrayReplace.ts
@@ -1,5 +1,8 @@
 function arrayReplace(inputArray: number[], elemToReplace: number, substitutionElem: number): number[] {
    
-}
+    return  inputArray.map( num => {
+        return num === elemToReplace ? substitutionElem : num
+    })
+ }
 
 console.log(arrayReplace([1, 2, 1], 1, 3));
