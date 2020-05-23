927a144c5d50bb306bb632bc1d3081971a2f833a
barbarajohnsonATX <johnson.barbara@gmail.com>
2020-05-22 22:48:06 -0500

    Done

--- arrayPreviousLess/arrayPreviousLess.ts
+++ arrayPreviousLess/arrayPreviousLess.ts
@@ -1,5 +1,14 @@
 function arrayPreviousLess(items: number[]): number[] {
 
+    let result = []
+     for(let i = items.length-1 ; i >= 0; i--) {
+        let smaller = items[i-1] < items[i] ? items[i-1] : -1
+        console.log(smaller)
+        result.unshift(smaller)
+
+    }
+    return result
+
 }
 
 console.log(arrayPreviousLess([3, 5, 2, 4, 5]));
