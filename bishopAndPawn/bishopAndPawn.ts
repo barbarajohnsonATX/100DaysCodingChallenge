a38d5214834c9c598d25d293081ab86f2f32b13e
barbarajohnsonATX <johnson.barbara@gmail.com>
2020-05-24 20:49:32 -0500

    Done

--- bishopAndPawn/bishopAndPawn.ts
+++ bishopAndPawn/bishopAndPawn.ts
@@ -1,25 +1,40 @@
-// a   1  2  3  4  5
-// b   1  2  3  4  5
-// c   1  2  3  4  5
-// d   1  2  3  4  5
-// ['a1', 'c3'] true 
-// ['a1', 'c2'] false 
+ 
 
 function bishopAndPawn(bishop: string, pawn: string): boolean {
+    const board = {
+        'a': 1, 
+        'b': 2,
+        'c': 3,
+        'd': 4,
+        'e': 5,
+        'f': 6,
+        'g': 7,
+        'h': 8
+    }
+
+    const bX = board[bishop.split('')[0]]
+    const bY = parseInt(bishop.split('')[1])
+    console.log(bX, bY)
      
-     return isDiagonal(bishop.split(''), pawn.split(''))
+    const pX = board[pawn.split('')[0]]
+    const pY = parseInt(pawn.split('')[1])
+    console.log(pX, pY)
+
+     return (Math.abs(bX - pX) === Math.abs(bY - pY))
 
 }
 
-function isDiagonal(a: number[], b: number[]): boolean {
- 
-    let alpha = a[0].charCodeAt(0) - b[0].charCodeAt(0);
-     console.log(alpha)
-     let num = parseInt(a[1]) - parseInt(b[1]);
-     console.log(num)
-     return (Math.abs(alpha) ===  Math.abs(num));
-         
- }
+//a1 b1 c1 d1 e1 f1 g1 h1
+//a2 b2 c2 d2 e2 f2 g2 h2
+//a3 b3 c3 d3 e3 f3 g3 h3
+//a4 b4 c4 d4 e4 f4 g4 h4
+//a5 b5 c5 d5 e5 f5 g5 h5
+//a6 b6 c6 d6 e6 f6 g6 h6
+//a7 b7 c7 d7 e7 f7 g7 h7
+//a8 b8 c8 d8 e8 f8 g8 h8
+
 
 console.log(bishopAndPawn('a1', 'c3'));
 console.log(bishopAndPawn('a1', 'c2'));
+console.log(bishopAndPawn('c8', 'f5'));
+console.log(bishopAndPawn('a2', 'f5'));
