20b2acf3bab3828b5b12c2a8515796e0d4a31f0a
barbarajohnsonATX <johnson.barbara@gmail.com>
2020-05-26 16:27:33 -0500

    Done

--- caseInsensitivePalimdrome/caseInsensitivePalindrome.ts
+++ caseInsensitivePalimdrome/caseInsensitivePalindrome.ts
@@ -1,6 +1,14 @@
 function isCaseInsensitivePalindrome(inputString: string): boolean {
 
+    let reversed = inputString.toLowerCase().split('').reverse().join('');
+    console.log(reversed)
+    return reversed === inputString.toLowerCase();
+ 
+
+
 }
 
 console.log(isCaseInsensitivePalindrome('AaBaa'));
 console.log(isCaseInsensitivePalindrome('abac'));
+console.log(isCaseInsensitivePalindrome('abbA'));
+console.log(isCaseInsensitivePalindrome('bacaaCAB'));
