0ed7b9a2b44d2f80541db1e9fd2bc27b74881d67
barbarajohnsonATX <johnson.barbara@gmail.com>
2020-05-26 22:02:04 -0500

    Done

--- commonCharacterCount/commonCharacterCount.ts
+++ commonCharacterCount/commonCharacterCount.ts
@@ -1,36 +1,30 @@
 function commonCharacterCount(s1: string, s2: string): number {
-    let s1Obj = {}
-    let s2Obj = {}
-
-    let s1Array = s1.split('');
-    let s2Array = s2.split('');
-    for(let c of s1Array) {
-        s1Obj[c] = s1Obj[c]+ 1 || 1;
-    }
-
-   for(let c of s2Array) {
-        s2Obj[c] = s2Obj[c]+ 1 || 1;
-    }
+    let s1Obj = getCharsList(s1);
+    let s2Obj = getCharsList(s2);
 
     let s1Keys = Object.keys(s1Obj)
      
-    let common = []
+    let count = 0;
+
     for(let key of s1Keys) {
          if(s2Obj.hasOwnProperty(key)) {
-             common.push(key)
+             count = count + Math.min(s1Obj[key], s2Obj[key])
+
          }
         
     }
-     console.log(common)
-
-
-   let count = 0;
-   for(let i = 0; i < common.length; i++) {
-       count = count + Math.min(s1Obj[common[i]], s2Obj[common[i]])
-   }
    
+   console.log(count)
    return count
 
+}
+
+function getCharsList(str) {
+    let obj = {}
+    for(let c of str.split('')) {
+        obj[c] = obj[c]+ 1 || 1;
+    }
+    return obj
 }
 
 console.log(commonCharacterCount('aabcc', 'adcaa'));
