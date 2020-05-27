3424e0470d4af0704d523da894657a7a40174521
barbarajohnsonATX <johnson.barbara@gmail.com>
2020-05-26 22:44:34 -0500

    Done

--- compareIntegers/compareIntegers.ts
+++ compareIntegers/compareIntegers.ts
@@ -1,7 +1,9 @@
 function compareIntegers(a: string, b: string): string {
 
+    return parseInt(a) > parseInt(b) ? "greater" : (parseInt(a) == parseInt(b) ? "equal" : "less"))
 }
 
 console.log(compareIntegers('12', '13'));
 console.log(compareIntegers('875', '799'));
 console.log(compareIntegers('1000', '1000'));
+console.log(compareIntegers('100', '1000'));
