8e0c91698ee9dfbcda63048526a2061c4bafd0e9
barbarajohnsonATX <johnson.barbara@gmail.com>
2020-05-27 01:05:56 -0500

    Done

--- containsDuplicates/containsDuplicates.ts
+++ containsDuplicates/containsDuplicates.ts
@@ -1,5 +1,16 @@
 function containsDuplicates(a: number[]): boolean {
+     let list = getList(a)
+     return Object.values(list).find(val => val > 1) ? true : false
 
+
+}
+
+function getList(arr) {
+    let obj = {};
+    for(let i = 0; i < arr.length; i++) {
+        obj[arr[i]] = obj[arr[i]] + 1 || 1;
+    }
+    return obj
 }
 
 console.log(containsDuplicates([1, 2, 3, 1]));
