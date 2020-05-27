6ad9c8fb76153da3245a20850ee55e10fb68293b
barbarajohnsonATX <johnson.barbara@gmail.com>
2020-05-26 22:37:25 -0500

    Done

--- companyBotStrategy/companyBotStrategy.ts
+++ companyBotStrategy/companyBotStrategy.ts
@@ -1,4 +1,18 @@
 function companyBotStrategy(trainingData: number[][]): number {
+
+    //nx2 array
+    //n = trainingData.length, 2 = trainingData[0].length
+    //filter for trainingData[0] === 1, result in correct array
+    //reduce to add to accum, increment 
+    //return sum/correct.length 
+
+ 
+    let correct = trainingData.filter( trainer => trainer[1] === 1);
+    console.log(correct)
+
+    let total = correct.reduce( (accum,current) =>  accum + current[0] , 0)                      
+    return (correct.length > 0 ? (total/correct.length).toFixed(1): 0);
+
  
 }
 
