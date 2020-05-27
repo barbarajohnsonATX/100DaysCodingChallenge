f9acb0ebe5428c47b2f2e35db0895ea5d514f980
barbarajohnsonATX <johnson.barbara@gmail.com>
2020-05-27 12:47:41 -0500

    Done

--- sumAllPrimes/sumAllPrimes.ts
+++ sumAllPrimes/sumAllPrimes.ts
@@ -1,5 +1,26 @@
 function sumAllPrimes(num: number): number {
 
+    let n = 2;
+    let sum = 0;
+
+    while(n <= num) {
+        sum = isPrime(n) ? sum + n : sum; 
+        n++;
+    }
+    //console.log(sum)
+    return sum
+
+}
+
+function isPrime(num) {
+    const divisors = [];
+    for(let i = 2; i < num ; i ++) {
+        if(num % i === 0) {
+            divisors.push(i)
+        }
+    }
+    return !divisors.length || false
+
 }
 
 console.log(sumAllPrimes(10));
