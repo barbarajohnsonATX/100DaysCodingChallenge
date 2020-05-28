b3208fe1f6454c08a910aa3af1b2b7abd7e81190
barbarajohnsonATX <johnson.barbara@gmail.com>
2020-05-27 20:01:32 -0500

    Done with recursive

--- depositProfit/depositProfit.ts
+++ depositProfit/depositProfit.ts
@@ -1,5 +1,25 @@
-function depositProfit(deposit: number, rate: number, threshold: number): number {
+function depositProfit(deposit: number, rate: number, threshold: number, year: number = 0): number {
  
+    let amount = deposit;
+    console.log(amount)
+   /* let year = 0;
+
+    while(amount < threshold) {
+        amount = amount * ( 1 + rate/100);
+        year++
+
+    }
+    console.log(year)
+    return year ; */
+
+    if(amount > threshold) {
+        return year;
+    } else {
+        year++;
+    }
+
+    return depositProfit(amount * ( 1 + rate/100), rate, threshold, year)
+    
 }
 
 console.log(depositProfit(100, 20, 170))
