1d40533b7b8211552e349892714e5c6d95c5d740
barbarajohnsonATX <johnson.barbara@gmail.com>
2020-05-27 23:00:23 -0500

    Done

--- digitDegree/digitDegree.ts
+++ digitDegree/digitDegree.ts
@@ -1,7 +1,26 @@
 function digitDegree(n: number): number {
+
+    //1. convert to array of strings, then to int digits
+    //2. if array length > 1, sum array elemts, repeat 
+
+    let nArray = n.toString().split('').map(n => parseInt( n));
+    console.log(nArray);
+    let degree = 0;
+
+    while(nArray.length > 1) {
+        let reduce = nArray.reduce( (accum, item) => accum + item, 0)
+        console.log(reduce)
+        nArray = [reduce].toString().split('').map(n => parseInt(n))
+        console.log(nArray)
+        degree++
+
+     }
+
+     return degree;
+  
   
 }
 
 console.log(digitDegree(5));
-console.log(digitDegree(10));
+console.log(digitDegree(100));
 console.log(digitDegree(91));
