d48032d2b6b42c35d1797fc236bc756928a6678e
barbarajohnsonATX <johnson.barbara@gmail.com>
2020-05-28 10:47:26 -0500

    Done

--- firstDigit/firstDigit.ts
+++ firstDigit/firstDigit.ts
@@ -1,6 +1,13 @@
 function firstDigit(inputString: string): string {
 
-}
+    let inputArray = inputString.split('')
+    for(let n of inputArray){
+        console.log(parseInt(n))
+        if(parseInt(n) == n) {
+            return n 
+        }  
+    }
+ }
 
 console.log(firstDigit('var_1__Int'));
 console.log(firstDigit('q2q-q'));
