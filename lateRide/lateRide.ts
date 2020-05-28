55ea8b62ee296519ee9802ad6a22f33ade4dffe1
barbarajohnsonATX <johnson.barbara@gmail.com>
2020-05-27 20:31:33 -0500

    Done

--- lateRide/lateRide.ts
+++ lateRide/lateRide.ts
@@ -1,6 +1,18 @@
 function lateRide(n: number): number {
+    const time = convertMinToArray(n)
+    console.log(time)
+    let result = time.toString().split('').filter(item => item != ',').map(item => parseInt(item))
+
+    console.log(result)
+    return result.reduce( (accum, item) => accum + item, 0);
 
 }
+
+function convertMinToArray(min){
+    let hours = Math.floor(min/60);
+    let minutes = min - 60 * hours
+    return [hours, minutes]
+ }
 
 console.log(lateRide(240));
 console.log(lateRide(808));
