d0174f5ad348eb78f98f9e048ff7c6045c05fcad
barbarajohnsonATX <johnson.barbara@gmail.com>
2020-05-28 10:38:10 -0500

    Done

--- sumOddFibonacciNums/sumOddFibonacciNums.ts
+++ sumOddFibonacciNums/sumOddFibonacciNums.ts
@@ -1,7 +1,22 @@
 function sumOddFibonacciNums(num: number): number {
+//1, 1, 2, 3, 5, 8, 13
+//sumFibs(10) sum of all odd fib nums less than 10 = 10 (1 + 1 + 3 + 5)
 
+    let fib = [1, 1];
+    let i = 2;
+
+    while(1) {
+        let sum = fib[i-1] + fib[i-2];
+        if(sum > num) {
+            return fib.filter(e => e % 2 !== 0).reduce( (accum, item) => accum + item, 0);
+        }
+
+        fib[i++] = sum;
+    }
+ 
 }
 
 console.log(sumOddFibonacciNums(1000));
 console.log(sumOddFibonacciNums(4000000));
-console.log(sumOddFibonacciNums(10));
+console.log(sumOddFibonacciNums(10));//
+ 
