89374e48956ebd7f981e53d8011fc6d0acd6b60b
barbarajohnsonATX <johnson.barbara@gmail.com>
2020-05-27 19:41:47 -0500

    Done

--- crossingSum/crossingSum.ts
+++ crossingSum/crossingSum.ts
@@ -1,4 +1,19 @@
 function crossingSum(matrix: number[][], a: number, b: number): number {
+
+    let intersection = matrix[a][b]
+ 
+    let sum = 0;
+    for(let i = 0; i < matrix[0].length; i++) {
+        sum += matrix[a][i];
+        console.log(sum)
+    }
+
+    for(let j = 0; j < matrix.length; j++){
+        sum += matrix[j][b];
+        console.log(sum)
+    }
+
+    return sum - intersection
   
 }
 
