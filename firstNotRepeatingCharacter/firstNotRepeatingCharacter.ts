1bacdc6483066842f666b0b0489447cfa5286061
barbarajohnsonATX <johnson.barbara@gmail.com>
2020-05-28 11:05:06 -0500

    Done

--- firstNotRepeatingCharacter/firstNotRepeatingCharacter.ts
+++ firstNotRepeatingCharacter/firstNotRepeatingCharacter.ts
@@ -1,6 +1,19 @@
 function firstNotRepeatingCharacter(s: string): string {
-
+  let list = getList(s);
+  let nonRepeatedChars = Object.keys(list).filter( key => list[key] === 1);
+  let indexes = nonRepeatedChars.map(ch => s.indexOf(ch)).sort( (a, b) => a - b);
+  return s[indexes[0]] || '_'
 }
+
+function getList(s) {
+    let obj = {};
+    for(let i = 0; i < s.length; i++) {
+        obj[s[i]] = obj.hasOwnProperty(s[i]) ? obj[s[i]] + 1 : 1
+    }
+    return obj;
+}
+
+
 
 console.log(firstNotRepeatingCharacter('abacabad'));
 console.log(firstNotRepeatingCharacter('abacabaabacaba'));
