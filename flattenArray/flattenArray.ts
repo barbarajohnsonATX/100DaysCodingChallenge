feedcd79e3b65400767ae68933de6409e2bbe273
barbarajohnsonATX <johnson.barbara@gmail.com>
2020-05-27 21:59:14 -0500

    Done

--- flattenArray/flattenArray.ts
+++ flattenArray/flattenArray.ts
@@ -1,6 +1,10 @@
 function flattenArray(arr: any[]): any[] {
-
+        const flat = [].concat(...arr);
+        console.log("flat", flat)
+        return flat.some(Array.isArray) ? flattenArray(flat) : flat;
+        
 }
+
 
 console.log(flattenArray([[["a"]], [["b"]]]));
 console.log(flattenArray([1, [2], [3, [[4]]]]));
