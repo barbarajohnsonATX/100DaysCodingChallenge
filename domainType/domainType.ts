00f1adb8cbf1d61cf1595a96a34bb16e14944811
barbarajohnsonATX <johnson.barbara@gmail.com>
2020-05-27 23:15:29 -0500

    Done

--- domainType/domainType.ts
+++ domainType/domainType.ts
@@ -1,4 +1,23 @@
 function domainType(domains: string[]): string[] {
+    const dot = {
+        'com': 'commercial',
+        'net': 'nework',
+        'info': 'information',
+        'org': 'organization'
+    }
+
+    let types = [];
+
+    console.log(domains);
+    for(let item of domains) {
+        let domainArray = item.split('.');
+     //    console.log(domainArray);
+         let ext = domainArray[domainArray.length - 1];
+     //    console.log(ext)
+         types.push(dot[ext])
+    }
+
+   return types;
 
 }
 
