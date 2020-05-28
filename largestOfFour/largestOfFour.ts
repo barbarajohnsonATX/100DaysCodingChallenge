a52ec546ea7f7acec0e937ec62709b43aa34ac4e
barbarajohnsonATX <johnson.barbara@gmail.com>
2020-05-27 20:14:33 -0500

    Done

--- largestOfFour/largestOfFour.ts
+++ largestOfFour/largestOfFour.ts
@@ -1,5 +1,14 @@
 function largestOfFour(nums: number[][]): number[] {
 
+    const largest = [];
+
+    for(let elem of nums){
+         largest.push(Math.max(...elem));
+    }
+
+  //  console.log(largest)
+    return largest
+
 }
 
 console.log(largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]) );
