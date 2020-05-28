381fbf316f77c1a6c7e9e7abd04adab689708e7e
barbarajohnsonATX <johnson.barbara@gmail.com>
2020-05-27 20:08:11 -0500

    Done

--- differentSymbolsNaive/differentSymbolsNaive.ts
+++ differentSymbolsNaive/differentSymbolsNaive.ts
@@ -1,5 +1,15 @@
 function differentSymbolsNaive(s: string): number {
- 
+    const list = getList(s)
+    console.log(list)
+    return Object.keys(list).length;
+}
+
+function getList(s) {
+    const obj = {};
+    for(let letter of s.split('')) {
+        obj[letter] = obj[letter]++ || 1;
+    }
+    return obj;
 }
 
 console.log(differentSymbolsNaive('cabca'));
